c59d12768fe820e607b8eb1685c503cf8e91e667
darkozlatev@sourcemxacademy.mk <darkozlatev@sourcemxacademy.mk>
2020-06-17 17:44:03 +0200

    da se dava avtor kaj article

--- src/routes/articles/getArticle.js
+++ src/routes/articles/getArticle.js
@@ -3,7 +3,9 @@ const { readJsonData, changeDate } = require('../../utils');
 module.exports = async (req, res) => {
   const { articleId } = req.params;
   const articles = await readJsonData('articles.json');
-  const article = articles.find(({ id }) => id === articleId) //da se najde article so toa id
+  const article = articles.find(({ id }) => id === articleId); //da se najde article so toa id
+  const authors = await readJsonData('authors.json');
+  article.author = authors.find(({ id }) => id === article.authorId); //da se najde avtor na article
   article.date = await changeDate(article.date); //da se formatira datata na article
   res.json(article);
 };
